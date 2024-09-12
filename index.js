e36ea099c614ee5b466a0e5fc8a32cab8cf17a46
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-09-12 13:44:21 +0200

    Event Listener for drawing

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+// Init Grid to draw on
 const containerDiv = document.querySelector(".container");
 generateGrid();
 
+// Get Grid Cells
+const gridCells = document.querySelectorAll("#grid");
+gridCells.forEach(function(node) {
+    node.addEventListener("mouseenter", () => {
+        node.style['background-color'] = "red";
+    });
+});
+
 
 function generateGrid() {
-    for(let i = 0; i < 16; i++) {
+    for(let i = 0; i < 255; i++) {
         const gridDiv = document.createElement("div");
         gridDiv.id = "grid";
         containerDiv.appendChild(gridDiv);
     }
 }
+
