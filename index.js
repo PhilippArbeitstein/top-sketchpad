c3e070e7f75962c9ae2fcb44f9a1fdaebd28a6e7
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-09-12 14:32:07 +0200

    New Sketchboard Generation

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-// Init Grid to draw on
 const containerDiv = document.querySelector(".container");
 generateGrid();
 
-// Get Grid Cells
-const gridCells = document.querySelectorAll("#grid");
-gridCells.forEach(node => {
-    node.addEventListener("mouseenter", () => {
-        node.style['background-color'] = "red";
-    });
-});
+const createBtn = document.querySelector(".create");
+createBtn.addEventListener("click", getUserInput);
 
+function generateGrid(squaresPerSide = 16) {
+  for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+    const gridDiv = document.createElement("div");
+    gridDiv.id = "grid";
+    containerDiv.appendChild(gridDiv);
+  }
 
-function generateGrid() {
-    for(let i = 0; i < 255; i++) {
-        const gridDiv = document.createElement("div");
-        gridDiv.id = "grid";
-        containerDiv.appendChild(gridDiv);
-    }
+  drawOnGrid();
 }
 
+function drawOnGrid() {
+  const gridCells = document.querySelectorAll("#grid");
+  gridCells.forEach((node) => {
+    node.addEventListener("mouseenter", () => {
+      node.style["background-color"] = "red";
+    });
+  });
+}
+
+function eraseGrid() {
+  const gridCells = document.querySelectorAll("#grid");
+  gridCells.forEach((node) => {
+    node.remove();
+  });
+}
+
+function getUserInput() {
+  const gridSize = Number(prompt("Enter new Sketchpad Size"));
+  if (gridSize <= 100 && typeof gridSize === "number") {
+    eraseGrid();
+    generateGrid(gridSize);
+  } else {
+    getUserInput();
+  }
+}
